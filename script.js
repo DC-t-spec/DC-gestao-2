bd371bb9d4d40afedbee74f35dfa495f4ab9a9a7
DC-t-spec <dcnet025@gmail.com>
2026-01-18 22:43:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3631,9 +3631,13 @@ alert("✅ Gestão Fácil---DC--- ");
       try {renderHome();} catch (e) {console.error("renderHome() falhou:", e);}
     });
   }
-    if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js").catch(console.error);
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(() => console.log("✅ Service Worker registado"))
+      .catch((err) => console.error("❌ Erro SW:", err));
   });
 }
+
 })();
